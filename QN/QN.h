f0785582fa094b8d718754b5ae5fb74b7e0230f3
冯勇海 <2206728426@qq.com>
2017-10-07 21:08:55 +0800

    支持carthage

---
+++ QN/QN.h
@@ -0,0 +1,25 @@
+//
+//  QN.h
+//  QN
+//
+//  Created by 冯勇海 on 2017/10/7.
+//  Copyright © 2017年 Chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QN.
+FOUNDATION_EXPORT double QNVersionNumber;
+
+//! Project version string for QN.
+FOUNDATION_EXPORT const unsigned char QNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QN/PublicHeader.h>
+
+#import <QN/QNConfiguration.h>
+#import <QN/QNFileRecorder.h>
+#import <QN/QNPipeline.h>
+#import <QN/QNResponseInfo.h>
+#import <QN/QNUploadManager.h>
+#import <QN/QNUploadOption.h>
+#import <QN/QNUrlSafeBase64.h>
